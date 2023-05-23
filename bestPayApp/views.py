bac778eefdf9fd63f8f22cab8501ca03ef4cc502
mikegyamfi <codeage20@gmail.com>
2023-05-23 14:40:30 +0000

    maintenance

--- bestPayApp/views.py
+++ bestPayApp/views.py
@@ -1,32 +1,34 @@
 import requests
 from django.shortcuts import render, redirect
 from bestPayApp.forms import SendMessageForm
+from django.http import HttpResponse, JsonResponse
 
 
 # Create your views here.
 def home(request):
-    form = SendMessageForm()
-    if request.user.is_authenticated:
-        form = SendMessageForm(
-            initial={
-                'name': request.user.username,
-                'email': request.user.email
-            }
-        )
-    if request.method == "POST":
-        form = SendMessageForm(request.POST)
-        if form.is_valid():
-            name = str(form.cleaned_data["name"])
-            email = str(form.cleaned_data["email"])
-            message = str(form.cleaned_data["message"])
+    # form = SendMessageForm()
+    # if request.user.is_authenticated:
+    #     form = SendMessageForm(
+    #         initial={
+    #             'name': request.user.username,
+    #             'email': request.user.email
+    #         }
+    #     )
+    # if request.method == "POST":
+    #     form = SendMessageForm(request.POST)
+    #     if form.is_valid():
+    #         name = str(form.cleaned_data["name"])
+    #         email = str(form.cleaned_data["email"])
+    #         message = str(form.cleaned_data["message"])
 
-            url = f"https://sms.arkesel.com/sms/api?action=send-sms&api_key=UmpEc1JzeFV4cERKTWxUWktqZEs&to=0242442147&from=BP-Site&sms={name}\n{email}\n{message}"
+    #         url = f"https://sms.arkesel.com/sms/api?action=send-sms&api_key=UmpEc1JzeFV4cERKTWxUWktqZEs&to=0242442147&from=BP-Site&sms={name}\n{email}\n{message}"
 
-            response = requests.request("GET", url)
-            print(response.text)
-            print(message)
-    context = {'form': form}
-    return render(request, 'layouts/index.html', context=context)
+    #         response = requests.request("GET", url)
+    #         print(response.text)
+    #         print(message)
+    # context = {'form': form}
+    # return render(request, 'layouts/index.html', context=context)
+    return HttpResponse("Site under maintenance. Try again later.")
 
 
 def services(request):
